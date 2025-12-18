6f6128397aa2a312d92e137d6c176407b8b029f5
Archan-Maru <archanmaru1710@gmail.com>
2025-12-18 17:28:35 +0530

    intialized basic project settings and added a user controller

--- src/db/index.js
+++ src/db/index.js
@@ -1,17 +1,15 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constant.js";
+import { DB_NAME } from "../constants.js";
 
-const connectDB=async ()=>{
-     try {
-            const coonectInstance= await  mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-    
-            console.log(`MongoDB connected \n
-                DB INSTANCE: ${coonectInstance.connection.host}`)
-    
-        } catch (error) {
-            console.log("MongoDB connection error",error); 
-            process.exit(1);
-        }
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection FAILED ", error);
+        process.exit(1)
+    }
 }
 
 export default connectDB
